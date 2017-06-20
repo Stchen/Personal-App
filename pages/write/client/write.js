e02ff1b86eeb75b0a6075f9700a506e67d05195c
Steven <Stchen105@gmail.com>
2017-06-20 10:42:26 -0400

    interaction with user

--- pages/write/client/write.js
+++ pages/write/client/write.js
@@ -11,6 +11,21 @@ Template.addstory.events({
     instance.$('#storyname').val("");
     instance.$('#chapternumber').val("");
     //this empties the text boxes after you enter something
-    People.insert({storyname, chapter})
+    Story.insert({storyname:storyname, chapter:chapter,owner:Meteor.userId()});
+    console.dir(this);
+  }
+})
+
+Template.removestory.events({
+  'click span'(elt,instance){
+    console.dir(this);
+    //this find the mongoid so we can delete something
+    console.log(this);
+    //this used to only show the word Object
+    if(this.story.owner == Meteor.userID()){
+        Story.remove(this.story._id);
+    }else{
+      alert("You do not have permission to do this.");
+    }
   }
 })
