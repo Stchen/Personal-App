67229968d8b246bb333da386646039b67fbc4775
Steven <Stchen105@gmail.com>
2017-06-20 14:21:44 -0400

    Made it secure with meteor

--- pages/write/client/write.js
+++ pages/write/client/write.js
@@ -11,21 +11,31 @@ Template.addstory.events({
     instance.$('#storyname').val("");
     instance.$('#chapternumber').val("");
     //this empties the text boxes after you enter something
-    Story.insert({storyname:storyname, chapter:chapter,owner:Meteor.userId()});
+    var writen = {storyname:storyname, chapter:chapter,owner:Meteor.userId()};
+    Meteor.call('writen.insert',writen);
+    //Story.insert(write);
     console.dir(this);
   }
+})
+
+Template.removestory.helpers({
+  owner() {return (Meteor.userId() == this.story.owner)}
 })
 
 Template.removestory.events({
   'click span'(elt,instance){
     console.dir(this);
     //this find the mongoid so we can delete something
-    console.log(this);
     //this used to only show the word Object
-    if(this.story.owner == Meteor.userID()){
+    if(Meteor.userId() == this.story.owner){
+      Meteor.call('writen.remove',this.story);
+    }
+    /*
+    if(this.story.owner == Meteor.userId()){
         Story.remove(this.story._id);
     }else{
       alert("You do not have permission to do this.");
     }
+    */
   }
 })
